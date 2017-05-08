d563cd0ae9676e7196226cff9df18ad0c7cfbd95
aeroheim <benben123213@gmail.com>
2017-05-08 01:06:09 -0500

    bug fixes for animated css transitions

--- src/app/components/animated-css-transition.js
+++ src/app/components/animated-css-transition.js
@@ -46,10 +46,9 @@ class AnimatedCSSTransition extends React.Component
     {
         if (this.props !== nextProps)
         {
-            if (nextProps.show !== this.state.active)
-            {
-                this.transition(nextProps);
-            }
+            // NOTE: Setting state via transition() inside of here leads to a weird situation in which styles 
+            // aren't applied to the children correctly. A callback is used to circumvent this issue.
+            setTimeout(() => this.transition(nextProps), 50);
         }
     }
 
@@ -72,6 +71,14 @@ class AnimatedCSSTransition extends React.Component
     {
         const props = nextProps !== undefined ? nextProps : this.props;
 
+        if (this.isDone())
+        {
+            for (const key in this.finished)
+            {
+                this.finished[key] = false;
+            }
+        }
+
         for (const key in this.transitions)
         {
             if (this.transitions === this.inTransitions)
@@ -80,7 +87,7 @@ class AnimatedCSSTransition extends React.Component
             }
             else
             {
-                this.styles[key] = `${this.outTransitions[key]} ${!props.show ? this.outStyles[key] : ''}`;
+                this.styles[key] = `${this.inTransitions[key]} ${this.inStyles[key]} ${this.outTransitions[key]} ${!props.show ? this.outStyles[key] : ''}`;
             }
         }
 
@@ -97,27 +104,18 @@ class AnimatedCSSTransition extends React.Component
             if (e.target.className.includes(this.transitions[key]))
             {
                 this.finished[key] = true;
-            break;
+                break;
             }
         }
 
         if(this.isDone())
         {
-            // swap transitions if necessary
-            if (this.props.show && this.transitions === this.inTransitions)
-            {
-                this.transitions = this.outTransitions;
-            }
-            else if (!this.props.show && this.transitions === this.outTransitions)
-            {
-                this.transitions = this.inTransitions;
-            }
+            this.transitions = this.props.show ? this.outTransitions : this.inTransitions;
 
             // reset styles
             for (const key in this.transitions)
             {
-                this.finished[key] = false;
-                this.styles[key] = this.transitions[key];
+                this.styles[key] = this.props.show ? `${this.inTransitions[key]} ${this.inStyles[key]} ${this.outTransitions[key]}`: this.inTransitions[key];
             }
 
             this.setState({
