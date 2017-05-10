9470b44294f64fe2311e1bb40b37ebf626dff7a5
Ben Pang <ben.pang@ni.com>
2017-05-10 14:05:26 -0500

    additional blog-list-item styling

--- src/app/components/blog-list-item.js
+++ src/app/components/blog-list-item.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import LinkButton from './link-button';
 import AnimatedCSSTransition from './animated-css-transition';
 import styles from '../static/styles/components/blog-list-item.css';
 
-const BlogListItem = ({ post, match }) =>
+const BlogListItem = ({ post, match, url }) =>
 {
     const inTransitions =
     {
@@ -31,12 +32,14 @@ const BlogListItem = ({ post, match }) =>
             {({ transitionStyles, onTransitionEnd }) => {
                 return (
                     <li className={`${styles.post} ${transitionStyles['post']}`} onTransitionEnd={onTransitionEnd}>
-                        <div className={styles.postColorBar}/>
-                        <div className={styles.postText}>
-                            <span className={styles.postTitle}>{post.title}</span>
-                            <span className={styles.postDate}>{monthFormatter.format(post.date).toUpperCase()} {post.date.getDate()}<br/>{post.date.getFullYear()}</span>
-                            <p className={styles.postDescription}>{post.description}</p>
-                        </div>
+                        <LinkButton link='/' className={styles.linkButton}>
+                            <div className={styles.postColorBar}/>
+                            <div className={styles.postText}>
+                                <span className={styles.postTitle}>{post.title}</span>
+                                <span className={styles.postDate}>{monthFormatter.format(post.date).toUpperCase()} {post.date.getDate()}<br/>{post.date.getFullYear()}</span>
+                                <p className={styles.postDescription}>{post.description}</p>
+                            </div>
+                        </LinkButton>
                     </li>);
             }}
         </AnimatedCSSTransition>
