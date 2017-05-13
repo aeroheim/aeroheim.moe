15980a3cc931026ae1300831c902cee00ea2f806
aeroheim <benben123213@gmail.com>
2017-05-13 13:16:58 -0500

    added header nav buttons

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import HeaderButton from './header-button';
 import styles from '../static/styles/components/header.css';
 
 const Header = () =>
 {
     return (
-        <div className={styles.bar}/>
+        <nav className={styles.navBar}>
+            <HeaderButton text='home' exact path='/' className={styles.headerButton} color={styles.homeColor}/>
+            <HeaderButton text='moonlight' exact path='/moonlight' className={styles.headerButton} color={styles.moonlightColor}/>
+            <HeaderButton text='bumps' path='/bumps' className={styles.headerButton} color={styles.bumpsColor}/>
+            <HeaderButton text='blog' path='/blog' className={styles.headerButton} color={styles.blogColor}/>
+            <HeaderButton text='about' exact path='/about' className={styles.headerButton} color={styles.aboutColor}/>
+        </nav>
     );
 }
 
