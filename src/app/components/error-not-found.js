84273a849e5b3a6a29f5655d840b9cdad75a1e89
Ben Pang <ben.pang@ni.com>
2017-05-19 17:03:05 -0500

    WIP fix error-not-found being unmounted

--- src/app/components/error-not-found.js
+++ src/app/components/error-not-found.js
@@ -25,7 +25,7 @@ const ErrorNotFound = (props) =>
     }
 
     return (
-        <AnimatedCSSTransition inTransitions={inTransitions} inStyles={inStyles} outTransitions={outTransitions} outStyles={outStyles} show={props.match}>
+        <AnimatedCSSTransition inTransitions={inTransitions} inStyles={inStyles} outTransitions={outTransitions} outStyles={outStyles} show={props.matchCount === 0}>
             {({ transitionStyles, onTransitionEnd }) => {
                 return (
                     <div className={`${styles.content} ${transitionStyles['content']}`} onTransitionEnd={onTransitionEnd}>
--- src/app/components/switch-transition.js
+++ src/app/components/switch-transition.js
@@ -3,24 +3,31 @@ import { withRouter, matchPath } from 'react-router';
 import { Route } from 'react-router-dom';
 
 // Behaves the same as react-router's Switch, except that Routes using the 'children' prop are always rendered.
-// This is required to correctly animate components that use RouteTransitions, since they should always be rendered no matter what.
+// This is required to correctly animate components that use RouteTransitions, since they must be rendered at all times.
 const SwitchTransition = (props) =>
 {
-    let matchFound = false;
-    const childrenToRender = props.children.filter((route) => 
+    let matchCount = 0;
+
+    let childrenToRender = props.children.filter((route) => 
     {
-        if (!matchFound && matchPath(props.location.pathname, { path: route.props.path, exact: route.props.exact }))
+        if (matchCount === 0 && matchPath(props.location.pathname, { path: route.props.path, exact: route.props.exact }))
         {
-            matchFound = true;
+            ++matchCount;
             return true;
         }
 
         if (route.props.children)
         {
+            if (matchPath(props.location.pathname, { path: route.props.path, exact: route.props.exact }))
+            {
+                ++matchCount;
+            }
+
             return true;
         }
     });
 
+    childrenToRender = React.Children.map(childrenToRender, (child) => React.cloneElement(child, { matchCount: matchCount }));
     return <span>{childrenToRender}</span>;
 }
 
