7e3ae7b5dd68e9b81e8cd06d3e4fab7bd4cc8174
aeroheim <benben123213@gmail.com>
2017-05-31 01:05:54 -0500

    refactored blog to use redux store

--- src/app/app.js
+++ src/app/app.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from './reducers/reducers';
 import Aeroheim from './components/aeroheim';
 import fonts from './static/styles/fonts/fonts.css';
 
+const store = createStore(
+    rootReducer,
+    applyMiddleware(thunkMiddleware),
+);
+
 document.getElementById("loadingLogo").remove();
-ReactDOM.render(
-    <BrowserRouter>
-        <Route path="/" component={Aeroheim}/>
-    </BrowserRouter>
-, document.getElementById("root"));
+render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Route path="/" component={Aeroheim}/>
+        </BrowserRouter>
+    </Provider>, 
+    document.getElementById("root")
+);
