27696f14998c3ec09e373f8678eef331122e8b38
aeroheim <benben123213@gmail.com>
2017-06-16 00:44:33 -0500

    syntax highlighting support for blog-post yay

--- src/app/util/blog-post-parser.js
+++ src/app/util/blog-post-parser.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { createElement } from 'react';
 import marksy from 'marksy/components';
+import highlight from 'highlight.js/lib/highlight';
+import hljs from 'highlight.js/lib/languages/javascript';
 import { BlogPostImageBlock, BlogPostImageGroup, BlogPostImage } from '../components/blog-post-image';
 
+highlight.registerLanguage('javascript', hljs);
+
 const parser = marksy({
+    createElement,
+    highlight,
     components: {
         ImageBlock(props) { return <BlogPostImageBlock {...props}/>; },
         ImageGroup(props) { return <BlogPostImageGroup {...props}/>; },
