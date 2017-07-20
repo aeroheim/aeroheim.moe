c82a1643731c594319e60f19b41b4af6a1db7dec
aeroheim <benben123213@gmail.com>
2017-07-19 21:46:17 -0500

    db regeneration on content change

--- src/server/backend.js
+++ src/server/backend.js
@@ -1,43 +1,62 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const chokidar = require('chokidar');
 const Datastore = require('nedb');
-const log = require('./log');
+const logger = require('./logger');
 const blog = require('./api/blog');
 
-function logError(error)
+function logError(err, msg)
 {
-    if (error !== undefined && error !== null)
+    if (err)
     {
-        console.log(error);
+        logger.log('error', msg);
     }
 }
 
 function initializeStoreFromDisk(store, path)
 {
-    store.insert(JSON.parse(fs.readFileSync(path, 'utf8')), (err) => logError(err));
+    logger.log('info', `Initializing store for: ${path}`);
+
+    // TODO: clear only data for related store.
+    store.remove({}, { multi: true }, (err) => logError(err, `Failed to clear db store: ${err}`));
+    store.insert(JSON.parse(fs.readFileSync(path, 'utf8')), (err) => logError(err, `Failed to populate db store: ${err}`));
 }
 
 module.exports = (PORT) => 
 {
+    // initialize db
     const db = {};
     db.blog = new Datastore();
     initializeStoreFromDisk(db.blog, blog.storePath);
 
+    // setup fs watcher for db
+    const watcher = chokidar.watch('./content/**/*.json');
+    watcher.on('change', (file) => 
+    {
+        console.log(file);
+        const storeName = path.basename(file, '.json');
+        if (storeName === 'blog')
+        {
+            logger.log('info', 'chokidar: blog.json modified');
+            initializeStoreFromDisk(db.blog, blog.storePath);
+        }
+    });
+
+    // initialize express server
     const app = express();
     app.locals.db = db;
+    app.use(logger.logExpress);
 
-    app.use(log.morgan);
+    // apply routes/middleware
     app.get('*.js', (req, res, next) =>
     {
         req.url = req.url + '.gz';
         res.set('Content-Encoding', 'gzip');
         next();
     });
-
     app.use('/', express.static(path.join(__dirname, '../../dist/')));
     app.use('/', blog.router);
-
     app.get('/favicon.ico', (req, res) => res.sendFile(path.join(__dirname, '..', 'favicon.ico')));
     app.get('/index.css', (req, res) => res.sendFile(path.join(__dirname, '..', 'index.css')));
     app.get('*', (req, res) => res.sendFile(path.join(__dirname, '..', 'index.html')));
