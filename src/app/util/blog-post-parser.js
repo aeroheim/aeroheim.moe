89f6b1a534220caab3704f6e715e3498d4c5ed37
aeroheim <benben123213@gmail.com>
2017-08-08 02:00:08 -0500

    blog styling for markdown elements

--- src/app/util/blog-post-parser.js
+++ src/app/util/blog-post-parser.js
@@ -4,6 +4,7 @@ import prism from 'prismjs';
 import prismjsx from 'prismjs/components/prism-jsx'
 import prismLanguages from 'prism-languages';
 import { BlogPostImageBlock, BlogPostImageGroup, BlogPostImage } from '../components/blog-post-image';
+import styles from '../static/styles/components/blog-post-elements.css';
 
 const parser = marksy({
     createElement,
@@ -17,6 +18,14 @@ const parser = marksy({
         ImageBlock(props) { return <BlogPostImageBlock {...props}/>; },
         ImageGroup(props) { return <BlogPostImageGroup {...props}/>; },
         Image(props) { return <BlogPostImage {...props}/>; },
+    },
+    elements:
+    {
+        h1({id, children}) { return <h1 id={id} className={styles.h1}>{children}</h1>; },
+        h2({id, children}) { return <h2 id={id} className={styles.h2}>{children}</h2>; },
+        h3({id, children}) { return <h3 id={id} className={styles.h3}>{children}</h3>; },
+        a ({href, title, target, children}) { return <a href={href} title={title} target={target} className={styles.a}>{children}</a>; },
+        hr() { return <hr className={styles.hr}/>; },
     },
 });
 
