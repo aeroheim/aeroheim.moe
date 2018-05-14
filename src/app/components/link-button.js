2c47a2ddb1fc441a13c138da50a90252897c14b9
aeroheim <benben123213@gmail.com>
2018-05-13 23:19:55 -0500

    refactoring link-button

--- src/app/components/link-button.js
+++ src/app/components/link-button.js
@@ -6,17 +6,13 @@ const LinkButton = ({ link, className, children }) =>
 {
     return new RegExp('^(?:[a-z]+:)?//', 'i').test(link)
         ? ( 
-            <a className={styles.link} href={link}>
-                <div className={className}>
-                    {children}
-                </div>
+            <a className={`${styles.link} ${className}`} href={link}>
+                {children}
             </a>
         )
         : ( 
-            <Link className={styles.link} to={link}>
-                <div className={className}>
-                    {children}
-                </div>
+            <Link className={`${styles.link} ${className}`} to={link}>
+                {children}
             </Link>
         );
 }
