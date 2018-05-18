3193fde7731bfa89711bf89f306352b991b4311c
aeroheim <benben123213@gmail.com>
2018-05-17 23:48:09 -0500

    update bio, refactor About styles

--- src/app/components/about.js
+++ src/app/components/about.js
@@ -72,35 +72,36 @@ class About extends React.Component
             <AnimatedCSSTransition inTransitions={inTransitions} inStyles={inStyles} outTransitions={outTransitions} outStyles={outStyles} show={this.props.match !== null}>
                 {({ transitionStyles }) => {
                     return (
-                        <div className={styles.page}>
-                            <div className={styles.content}>
-                                <PageHeader className={styles.headerStyle} color={styles.headerColor} show={this.props.match !== null}>ABOUT</PageHeader>
-                                <article className={`${styles.bio} ${transitionStyles['bio']}`}>
-                                    <h1 style={{marginBottom: 0}}>Benjamin Pang (龐天擇).</h1>
-                                    <ul>
-                                        <li>{`${age}-year-old Software Engineer @ Austin, TX`}</li>
-                                        <li>B.S Computer Science @ The University of Texas at Austin (2015)</li>
-                                        <li>一點中文/ENG OK</li>
+                        <div className={`${this.props.className} ${styles.content}`}>
+                            <PageHeader className={styles.header} color={styles.aboutColor} show={this.props.match !== null}>ABOUT</PageHeader>
+                            <article className={`${styles.bio} ${transitionStyles['bio']}`}>
+                                <h1 style={{marginBottom: 0}}>Benjamin Pang (龐天擇) :</h1>
+                                <p>
+                                    I'm a {`${age}`}-year-old software engineer from Austin, TX. Born in Canada, I spent part 
+                                    <br/>
+                                    of my childhood in Hong Kong and grew up in the states. I graduated from 
+                                    <br/>
+                                    The University of Texas at Austin with a B.S Computer Science in 2015.
+                                </p>
+                                <p>
+                                    I <i>dig</i> mechanical keyboards (average ~150WPM):
+                                    <ul className={styles.list}>
+                                        <li>Winkeyless 22mini-B w/ Hako Clears + EnjoyPBT Hiragana</li>
+                                        <li>Leopold FC660C w/ Hypersphere rings ♥</li>
                                     </ul>
-                                    <p>
-                                        My mechanical keyboards (~175WPM):
-                                        <ul>
-                                            <li>Whitefox ARIA w/ Gateron Blues</li>
-                                            <li>Leopold FC660C w/ Hypersphere rings ♥</li>
-                                        </ul>
-                                        Currently playing the following games:
-                                        <ul>
-                                            <li>Black Desert Online: Aeroheim family @ NA</li>
-                                            <li>Sound Voltex: grinding from SKILL LEVEL11 -> 12</li>
-                                        </ul>
-                                        Technologies I'm interested in right now:
-                                        <ul>
-                                            <li>React</li>
-                                            <li>Electron</li>
-                                        </ul>
-                                    </p>
-                                </article>
-                            </div>
+                                    I'm also a long-time MMO junkie, avid gamer, and otoge fan:
+                                    <ul className={styles.list}>
+                                        <li>Past: <i>Ragnarok Online, Granado Espada, Aion, FFXIV, Tree of Savior</i></li>
+                                        <li>Current: <i>Black Desert Online (NA) - Aeroheim family</i></li>
+                                        <li>Sound Voltex: sitting at ~9150 VOLFORCE 😭</li>
+                                    </ul>
+                                    You can hit me up at the following links:
+                                    <ul className={styles.list}>
+                                        <li><a className={styles.link} href='https://twitter.com/aeroheim'>twitter</a></li>
+                                        <li><a className={styles.link} href='mailto:aeroheim@gmail.com'>mail</a></li>
+                                    </ul>
+                                </p>
+                            </article>
                         </div>
                     );
                 }}
