54c6b3fb53d713fddaa16e699ea67a492921fd21
aeroheim <benben123213@gmail.com>
2019-01-21 19:23:11 -0600

    lint server and local scripts

--- src/server/api/blog.js
+++ src/server/api/blog.js
@@ -1,121 +1,107 @@
+/* eslint-disable no-shadow */
+/* eslint-disable consistent-return */
+/* eslint-disable no-underscore-dangle */
+
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { query } = require('express-validator/check');
 const { filterStaticContent, filterQuery, validateExpressValidator } = require('../middleware/validation');
-const blogContentPath = path.join(__dirname, '../../../content/blog');
 
+const blogContentPath = path.join(__dirname, '../../../content/blog');
 const DEFAULT_PAGINATION_LIMIT = 5;
 
 const validateBlogQuery = [
-    filterQuery((query) => ['page', 'limit'].includes(query)), 
-    query('limit').optional().isInt({ min: 0 }), 
-    query('page').optional().isInt({ min: 1 }),
-    validateExpressValidator,
+  filterQuery(q => ['page', 'limit'].includes(q)),
+  query('limit').optional().isInt({ min: 0 }),
+  query('page').optional().isInt({ min: 1 }),
+  validateExpressValidator,
 ];
 
-function handleBlogRequest(req, res)
-{
-    const limit = req.query.limit ? req.query.limit : DEFAULT_PAGINATION_LIMIT;
-    req.app.locals.db.blog.count({}, (err, count) =>
-    {
-        if (err)
-        {
-            return res.sendStatus(404);
+function handleBlogRequest(req, res) {
+  const limit = req.query.limit ? req.query.limit : DEFAULT_PAGINATION_LIMIT;
+  req.app.locals.db.blog.count({}, (err, count) => {
+    if (err) {
+      return res.sendStatus(404);
+    }
+
+    const pages = Math.ceil(count / limit); // pages are one-indexed
+    const page = req.query.page ? Math.min(req.query.page, pages) : 1;
+    req.app.locals.db.blog.find({}).sort({ date: -1 }).skip(limit * (page - 1)).limit(limit)
+      .exec((err, posts) => {
+        if (err) {
+          return res.sendStatus(404);
         }
 
-        // pages are one-indexed
-        const pages = Math.ceil(count / limit);
-        const page = req.query.page ? Math.min(req.query.page, pages) : 1;
-
-        req.app.locals.db.blog.find({}).sort({ date: -1 }).skip(limit * (page - 1)).limit(limit).exec((err, posts) =>
-        {
-            if (err)
-            {
-                return res.sendStatus(404);
-            }
-
-            return res.json({
-                posts: posts,
-                limit: limit,
-                page: page,
-                pages: pages
-            });
+        return res.json({
+          posts,
+          limit,
+          page,
+          pages,
         });
-    });
+      });
+  });
 }
 
 const validateBlogPostQuery = [
-    filterQuery((query) => ['limit'].includes(query)), 
-    query('limit').optional().isInt({ min: 0 }), 
-    validateExpressValidator,
+  filterQuery(q => ['limit'].includes(q)),
+  query('limit').optional().isInt({ min: 0 }),
+  validateExpressValidator,
 ];
 
-function handleBlogPostRequest(req, res)
-{
-    // find post
-    req.app.locals.db.blog.findOne({'_id': req.params.id}, (err, post) =>
-    {
-        if (err || !post)
-        {
-            return res.sendStatus(404);
+function handleBlogPostRequest(req, res) {
+  // find post
+  req.app.locals.db.blog.findOne({ _id: req.params.id }, (err, post) => {
+    if (err || !post) {
+      return res.sendStatus(404);
+    }
+
+    const json = Object.assign({}, post);
+    json.limit = req.query.limit ? req.query.limit : DEFAULT_PAGINATION_LIMIT;
+
+    // find previous post
+    req.app.locals.db.blog.find({ date: { $lt: post.date } }).sort({ date: -1 }).limit(1).exec((err, prevPost) => {
+      if (err) {
+        return res.sendStatus(404);
+      }
+
+      if (prevPost.length !== 0) {
+        json.prevPost = prevPost[0];
+      }
+
+      // find next post and current page of post.
+      req.app.locals.db.blog.find({ date: { $gt: post.date } }).sort({ date: 1 }).exec((err, newerPosts) => {
+        if (err) {
+          return res.sendStatus(404);
         }
 
-        const json = Object.assign({}, post);
-        json.limit = req.query.limit ? req.query.limit : DEFAULT_PAGINATION_LIMIT;
+        if (newerPosts.length !== 0) {
+          json.nextPost = newerPosts[0];
+        }
 
-        // find previous post
-        req.app.locals.db.blog.find({'date': { $lt: post.date }}).sort({ date: -1 }).limit(1).exec((err, prevPost) =>
-        {
-            if (err)
-            {
-                return res.sendStatus(404);
-            }
+        json.page = Math.floor(newerPosts.length / json.limit) + 1;
 
-            if (prevPost.length !== 0)
-            {
-                json.prevPost = prevPost[0];
-            }
+        // finally read post content.
+        fs.readFile(path.join(blogContentPath, `${post._id}/${post._id}.md`), 'utf8', (err, data) => {
+          if (err) {
+            return res.sendStatus(404);
+          }
 
-            // find next post and current page of post.
-            req.app.locals.db.blog.find({'date': { $gt: post.date }}).sort({ date: 1 }).exec((err, newerPosts) =>
-            {
-                if (err)
-                {
-                    return res.sendStatus(404);
-                }
-
-                if (newerPosts.length !== 0)
-                {
-                    json.nextPost = newerPosts[0];
-                }
-
-                json.page = Math.floor(newerPosts.length / json.limit) + 1;
-
-                // finally read post content.
-                fs.readFile(path.join(blogContentPath, `${post._id}/${post._id}.md`), 'utf8', (err, data) =>
-                {
-                    if (err)
-                    {
-                        return res.sendStatus(404);
-                    }
-
-                    json.content = data;
-                    return res.json(json);
-                });
-            });
+          json.content = data;
+          return res.json(json);
         });
+      });
     });
+  });
 }
 
 const router = express.Router();
-router.use('/blog/*', filterStaticContent((staticContent) => !staticContent.endsWith('.md') && !staticContent.endsWith('.json')));
+router.use('/blog/*', filterStaticContent(staticContent => !staticContent.endsWith('.md') && !staticContent.endsWith('.json')));
 router.use('/blog', express.static(blogContentPath));
 router.get('/api/blog', validateBlogQuery, handleBlogRequest);
 router.get('/api/blog/:id', validateBlogPostQuery, handleBlogPostRequest);
 
-module.exports = 
-{
-    router: router,
-    storePath: path.join(blogContentPath, 'blog.json')
-}
+module.exports = {
+  router,
+  storePath: path.join(blogContentPath, 'blog.json'),
+};
--- src/server/middleware/validation.js
+++ src/server/middleware/validation.js
@@ -1,63 +1,51 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-undef */
+
 const { validationResult } = require('express-validator/check');
 
-function filterStaticContent(filter, handler)
-{
-    return (req, res, next) =>
-    {
-        const staticContent = req.params['0'];
-        if (staticContent && !filter(staticContent))
-        {
-            if (handler)
-            {
-                return handler(staticContent, {req, res, next})
-            }
-            else
-            {
-                return res.status(403).send(`Access denied.`);
-            }
-        }
+function filterStaticContent(filter, handler) {
+  return (req, res, next) => {
+    const staticContent = req.params['0'];
+    if (staticContent && !filter(staticContent)) {
+      if (handler) {
+        return handler(staticContent, { req, res, next });
+      }
 
-        next();
-    };
-}
-
-function filterQuery(filter, handler)
-{
-    return (req, res, next) =>
-    {
-        for(query in req.query)
-        {
-            if (!filter(query))
-            {
-                if (handler)
-                {
-                    return handler(query, {req, res, next});
-                }
-                else
-                {
-                    return res.status(500).send(`Unsupported query: ${query}.`);
-                }
-            }
-        }
-
-        next();
-    };
-}
-
-function validateExpressValidator(req, res, next)
-{
-    const validationErrors = validationResult(req);
-    if (!validationErrors.isEmpty())
-    {
-        return res.status(422).json({ errors: validationErrors.array() });
+      return res.status(403).send('Access denied.');
     }
 
     next();
+  };
 }
 
-module.exports =
-{
-    filterStaticContent: filterStaticContent,
-    filterQuery: filterQuery,
-    validateExpressValidator: validateExpressValidator,
+function filterQuery(filter, handler) {
+  return (req, res, next) => {
+    for (query in req.query) {
+      if (!filter(query)) {
+        if (handler) {
+          return handler(query, { req, res, next });
+        }
+
+        return res.status(500).send(`Unsupported query: ${query}.`);
+      }
+    }
+
+    next();
+  };
 }
+
+function validateExpressValidator(req, res, next) {
+  const validationErrors = validationResult(req);
+  if (!validationErrors.isEmpty()) {
+    return res.status(422).json({ errors: validationErrors.array() });
+  }
+
+  next();
+}
+
+module.exports = {
+  filterStaticContent,
+  filterQuery,
+  validateExpressValidator,
+};
