7b83924a479bc340c3b8e91a9e7ccbf0a3aba6ee
aeroheim <benben123213@gmail.com>
2019-04-13 23:10:33 -0500

    change og:image to 512x512

--- src/app/components/meta.jsx
+++ src/app/components/meta.jsx
@@ -6,7 +6,7 @@ const Meta = ({ title, description }) => (
     <meta name="og:type" content="website" />
     <meta name="og:title" content={title} />
     <meta name="og:description" content={description} />
-    <meta name="og:image" content="https://aeroheim.moe/favicon-96x96.png" />
+    <meta name="og:image" content="https://aeroheim.moe/favicon-512x512.png" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="@aeroheim" />
   </Helmet>
